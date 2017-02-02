f1baa1d819ac96e4ecaef3d17fd8a65e903a8935
Kiyoaki Tsurutani <tsurutani_kiyoaki@cygames.co.jp>
2017-02-02 18:44:49 +0900

    Update nuget package version

--- src/BitFlyer.Apis/Properties/AssemblyInfo.cs
+++ src/BitFlyer.Apis/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("01865c9c-7b03-486f-976c-6af3976cdb3d")]
-[assembly: AssemblyVersion("2.1.1")]
-[assembly: AssemblyFileVersion("2.1.1")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
