b8ac74946314c6dd5cb3da416b21c4cc57aebce5
kiyoaki <kiyoaki.tsurutani@gmail.com>
2019-07-20 20:56:15 +0900

    Remove Unused

--- BitFlyer.Apis/BitFlyerConstants.cs
+++ BitFlyer.Apis/BitFlyerConstants.cs
@@ -5,6 +5,5 @@ namespace BitFlyer.Apis
     internal static class BitFlyerConstants
     {
         internal static readonly Uri BaseUri = new Uri("https://api.bitflyer.jp/");
-        internal const string SubscribeKey = "sub-c-52a9ab50-291b-11e5-baaa-0619f8945a4f";
     }
 }
